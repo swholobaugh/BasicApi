53a2e4ff7b43e37e299ecb70a70d3bd6a34fcee3
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-03-23 13:47:42 -0500

    Added packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import './helpers/dotenv';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'express-pino-logger';
 
 const app = express();
 const port = parseInt(process.env.PORT);
 
+app.use(cors({origin: process.env.ORIGIN}));
+app.use(helmet());
+app.use(pino());
+
 app.get('/', (req, res) => {
+    req.log.info('Calling root');
     res.json({msg: 'Hello There'})
 });
 
